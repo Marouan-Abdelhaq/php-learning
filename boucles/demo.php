93f99bfb484ec4630a81e22ee828a983082765a8
Marouan Abdelhaq <abdelhaqmarouan70@gmail.com>
2025-05-31 20:52:39 +0100

    boucles

--- boucles/demo.php
+++ boucles/demo.php
@@ -1,3 +1,18 @@
 <?php
+// $chiffre = NULL;
+// $heur = (int)readline('Entrer : ');
+
+// while ($chiffre !== 10)
+// {
+//     $chiffre = (int)readline('Entrer heur : ');
+//     break;
+// }
+
+// echo 'Bravo';
+
+for ($i = 0; $i < 10; $i++)
+{
+    echo "- $i \n";
+}
 
 ?>
